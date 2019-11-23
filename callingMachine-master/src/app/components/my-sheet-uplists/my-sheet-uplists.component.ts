5c4ed28bbaeeaf4a9ffb732c5b09bdb81e36857d
khashayar panbeian <kpanbeian@gmail.com>
2019-11-23 18:47:31 +0330

    new update

--- callingMachine-master/src/app/components/my-sheet-uplists/my-sheet-uplists.component.ts
+++ callingMachine-master/src/app/components/my-sheet-uplists/my-sheet-uplists.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators} from '@angular/forms';
-import { UplistsService } from 'src/app/services/uplists.service';
-import { pluck } from 'rxjs/operators';
-import { MatSnackBar } from '@angular/material';
-import { ActivatedRoute } from '@angular/router';
-
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { UplistsService } from "src/app/services/uplists.service";
+import { pluck } from "rxjs/operators";
+import { MatSnackBar } from "@angular/material";
+import { ActivatedRoute } from "@angular/router";
+import { MatDatepicker } from "@angular/material/datepicker";
 
 @Component({
-  selector: 'app-my-sheet-uplists',
-  templateUrl: './my-sheet-uplists.component.html',
-  styleUrls: ['./my-sheet-uplists.component.scss']
+  selector: "app-my-sheet-uplists",
+  templateUrl: "./my-sheet-uplists.component.html",
+  styleUrls: ["./my-sheet-uplists.component.scss"]
 })
 export class MySheetUplistsComponent implements OnInit {
   ngOnInit(): void {
@@ -18,40 +18,39 @@ export class MySheetUplistsComponent implements OnInit {
   formGroup: FormGroup;
   id: string;
 
+  constructor(
+    private UplistsService: UplistsService,
+    private router: ActivatedRoute,
+    private snackbar: MatSnackBar
+  ) {
+    this.formGroup = new FormGroup({
+      ListID: new FormControl("1", [Validators.required]),
+      AllRecords: new FormControl("1", [Validators.required]),
+      SuccessRecords: new FormControl("1", [Validators.required]),
+      Date: new FormControl("", [Validators.required]),
+      actions: new FormControl("", [Validators.required]),
+      CustomerID: new FormControl("", [Validators.required]),
+      department: new FormControl("", [Validators.required]),
+      add_date: new FormControl("", [Validators.required]),
+      Last_attempt_date: new FormControl("", [Validators.required]),
+      lock_call: new FormControl("", [Validators.required]),
+      call_status: new FormControl("", [Validators.required]),
+      call_duration: new FormControl("", [Validators.required]),
+      info: new FormControl("", [Validators.required])
+    });
+  }
 
-  constructor(private UplistsService: UplistsService, private router: ActivatedRoute, private snackbar: MatSnackBar) {
+  //   ngOnInit() {
+  //     this.router.params.pipe(pluck('id')).subscribe((id) => {
+  // 			this.id = id;
+  // 			this.UplistsService.get(id).subscribe((resp) => {
+  // 				this.formGroup.patchValue(resp);
+  //   });
+  // });
 
-  this.formGroup = new FormGroup({
-    ListID: new FormControl('1', [Validators.required]),
-    AllRecords: new FormControl('1', [Validators.required]),
-    SuccessRecords: new FormControl('1', [Validators.required]),
-    Date: new FormControl('', [Validators.required]),
-    actions: new FormControl('', [Validators.required]),
-    CustomerID: new FormControl('', [Validators.required]),
-    department: new FormControl('', [Validators.required]),
-    add_date: new FormControl('' , [Validators.required]),
-    Last_attempt_date: new FormControl('', [Validators.required]),
-    lock_call: new FormControl('', [Validators.required]),
-    call_status: new FormControl('', [Validators.required]),
-    call_duration: new FormControl('', [Validators.required]),
-    info: new FormControl('', [Validators.required])
-
-  });
-}
-
-//   ngOnInit() {
-//     this.router.params.pipe(pluck('id')).subscribe((id) => {
-// 			this.id = id;
-// 			this.UplistsService.get(id).subscribe((resp) => {
-// 				this.formGroup.patchValue(resp);
-//   });
-// });
-
-
-
-// // this.UplistsService.update(this.id, this.formGroup.value).subscribe((resp) => {
-// //   this.snackbar.open('اطلاعات ثبت شد ', null, { duration: 999 });
-// //   this.formGroup.patchValue(resp);
-// // });
-// }
+  // // this.UplistsService.update(this.id, this.formGroup.value).subscribe((resp) => {
+  // //   this.snackbar.open('اطلاعات ثبت شد ', null, { duration: 999 });
+  // //   this.formGroup.patchValue(resp);
+  // // });
+  // }
 }
--- callingMachine-master/src/app/up-lists/up-lists.component.ts
+++ callingMachine-master/src/app/up-lists/up-lists.component.ts
@@ -8,89 +8,92 @@ import { pluck } from 'rxjs/operators';
 import { ActivatedRoute } from '@angular/router';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { MySheetComponent } from '../components/my-sheet/my-sheet.component';
-import { MatBottomSheet, MatBottomSheetRef} from '@angular/material/bottom-sheet'
+import {
+  MatBottomSheet,
+  MatBottomSheetRef
+} from '@angular/material/bottom-sheet';
 import { MySheetUplistsComponent } from '../components/my-sheet-uplists/my-sheet-uplists.component';
 
 @Component({
-	selector: 'app-up-lists',
-	templateUrl: './up-lists.component.html',
-	styleUrls: [ './up-lists.component.scss' ]
+  selector: 'app-up-lists',
+  templateUrl: './up-lists.component.html',
+  styleUrls: ['./up-lists.component.scss']
 })
 export class UpListsComponent implements OnInit {
-	// tslint:disable-next-line: no-use-before-declare
+  // tslint:disable-next-line: no-use-before-declare
   dataSource = new MatTableDataSource<UpLists>([]);
-	id: string;
+  id: string;
 
-	displayedColumns: string[] = [
-		'ListID',
-		'CustomerID',
-		'info',
-		'AllRecords',
-		'SuccessRecords',
-		'department',
-		'Date',
-		'add_date',
-		'Last_attempt_date',
-		'lock_call',
-		'call_status',
-		'call_duration',
-		'actions'
-	];
+  displayedColumns: string[] = [
+    'ListID',
+    'CustomerID',
+    'info',
+    'AllRecords',
+    'SuccessRecords',
+    'department',
+    'Date',
+    'add_date',
+    'Last_attempt_date',
+    'lock_call',
+    'call_status',
+    'call_duration',
+    'actions'
+  ];
 
-	selectedDepartment: UpLists;
+  selectedDepartment: UpLists;
 
-	@ViewChild(MatPaginator, { static: true })
-	paginator: MatPaginator;
+  @ViewChild(MatPaginator, { static: true })
+  paginator: MatPaginator;
 
- formGroup: FormGroup;
-	constructor(private UpListsService: UplistsService, private snackbar: MatSnackBar, private router: ActivatedRoute, private buttomSheet: MatBottomSheet) {
-
-
-		this.UpListsService.list().subscribe((response) => {
+  formGroup: FormGroup;
+  constructor(
+    private UpListsService: UplistsService,
+    private snackbar: MatSnackBar,
+    private router: ActivatedRoute,
+    private buttomSheet: MatBottomSheet
+  ) {
+    this.UpListsService.list().subscribe((response) => {
       this.dataSource.data = response.Result;
 
       this.formGroup = new FormGroup({
-      ListID: new FormControl('1', [Validators.required]),
-      AllRecords: new FormControl('1', [Validators.required]),
-      SuccessRecords: new FormControl('1', [Validators.required]),
-      Date: new FormControl('', [Validators.required]),
-      actions: new FormControl('', [Validators.required]),
-      CustomerID: new FormControl('', [Validators.required]),
-      department: new FormControl('', [Validators.required]),
-      add_date: new FormControl('' , [Validators.required]),
-      Last_attempt_date: new FormControl('', [Validators.required]),
-      lock_call: new FormControl('', [Validators.required]),
-      call_status: new FormControl('', [Validators.required]),
-      call_duration: new FormControl('', [Validators.required]),
-      info: new FormControl('', [Validators.required])
+        ListID: new FormControl('1', [Validators.required]),
+        AllRecords: new FormControl('1', [Validators.required]),
+        SuccessRecords: new FormControl('1', [Validators.required]),
+        Date: new FormControl('', [Validators.required]),
+        actions: new FormControl('', [Validators.required]),
+        CustomerID: new FormControl('', [Validators.required]),
+        department: new FormControl(''),
+        add_date: new FormControl(''),
+        Last_attempt_date: new FormControl(''),
+        lock_call: new FormControl(''),
+        call_status: new FormControl(''),
+        call_duration: new FormControl(''),
+        info: new FormControl('')
+      });
     });
-		});
-	}
+  }
 
-
-  openBottomSheet()
-  {
+  openBottomSheet() {
     this.buttomSheet.open(MySheetUplistsComponent);
   }
 
-	remove(item: UpLists) {
-		debugger;
-		this.UpListsService.remove(item.ID).subscribe((res) => {});
-		debugger;
-		this.snackbar.open('رکورد مورد نظر حذف شد', null, { duration: 999 });
-		return;
-	}
+  remove(item: UpLists) {
+    debugger;
+    this.UpListsService.remove(item.ID).subscribe(res => {});
+    debugger;
+    this.snackbar.open('رکورد مورد نظر حذف شد', null, { duration: 999 });
+    return;
+  }
 
-	refresh(item: UpLists) {
-		this.UpListsService.refresh(item.ID).subscribe((res) => {});
-	}
+  // refresh(item: UpLists) {
+  //   this.UpListsService.refresh(item.ID).subscribe(res => {});
+  // }
 
-	onselect(id: number) {
-		console.log(id);
-		return id;
-	}
+  onselect(id: number) {
+    console.log(id);
+  }
 
-	ngOnInit() {
-		this.dataSource.paginator = this.paginator;
-	}
+  ngOnInit() {
+    this.dataSource.paginator = this.paginator;
+  }
 }
